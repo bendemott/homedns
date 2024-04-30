5ea2cde78dc065531a05f91791a72e1db3ee5559
Ben DeMott <ben.demott@gmail.com>
2024-04-29 23:42:18 -0600

    default config creates paths based on working directory
    Paths in the configuration are defaulted to the working directory.

--- homedns/config.py
+++ homedns/config.py
@@ -1,73 +1,84 @@
 import collections.abc
 import copy
+from os.path import join
+import os
+from typing import Any
 
-DEFAULT_SERVER_CONFIG = {
-    "http": None,
-    "https": {
-        "listen": 443,
-        "generate_keys": True,
-        "private_key": "/etc/homedns/server.pem",
-        "public_key": "/etc/homedns/server.crt"
-    },
-    "no_auth": {
-        "enabled": False
-    },
-    "jwt_auth": {
-        "enabled": True,
-        "algorithms": [
-            "RS256"
-        ],
-        "secrets_path": "/etc/homedns/jwt_secrets",
-        "issuer": "homedns-clients",
-        "audience": [
-            "homedns-api"
-        ],
-        "leeway": 30,
-        "options": {
-            "verify_exp": True,
-            "verify_nbf": True,
-            "verify_aud": True,
-            "verify_iss": True
-        }
-    },
-    "basic_auth": {
-        "enabled": False
-    },
-    "dns": {
-        "listen_tcp": 53,
-        "listen_udp": 53,
-        "cache": {
-            "enabled": True
-        },
-        "forwarding": {
-            "enabled": True,
-            "servers": [
-                "208.67.222.222",
-                "208.67.220.220"
-            ],
-            "timeouts": [
-                1,
-                3,
-                11,
-                30
-            ]
-        },
-        "database": {
-            "sqlite": {
-                "path": "/etc/homedns/records.sqlite"
-            }
-        },
-        "tld": 300,
-        "verbosity": 2
-    }
-}
 
 class ServerConfig:
     def __init__(self, config: dict):
         self._config = config
 
+    @classmethod
+    def get_default(cls, directory=None) -> dict[str, Any]:
+        if not directory:
+            directory = os.getcwd()
+
+        conf = {
+            "http": None,
+            "https": {
+                "listen": 443,
+                "generate_keys": True,
+                "private_key": join(directory, "server.pem"),
+                "public_key": join(directory, "server.crt"),
+            },
+            "no_auth": {
+                "enabled": False
+            },
+            "jwt_auth": {
+                "enabled": True,
+                "algorithms": [
+                    "RS256"
+                ],
+                "secrets_path": join(directory, "jwt_secrets"),
+                "issuer": "homedns-clients",
+                "audience": [
+                    "homedns-api"
+                ],
+                "leeway": 30,
+                "options": {
+                    "verify_exp": True,
+                    "verify_nbf": True,
+                    "verify_aud": True,
+                    "verify_iss": True
+                }
+            },
+            "basic_auth": {
+                "enabled": False
+            },
+            "dns": {
+                "listen_tcp": 53,
+                "listen_udp": 53,
+                "cache": {
+                    "enabled": True
+                },
+                "forwarding": {
+                    "enabled": True,
+                    "servers": [
+                        "208.67.222.222",
+                        "208.67.220.220"
+                    ],
+                    "timeouts": [
+                        1,
+                        3,
+                        11,
+                        30
+                    ]
+                },
+                "database": {
+                    "sqlite": {
+                        "path": join(directory, "records.sqlite")
+                    }
+                },
+                "tld": 300,
+                "verbosity": 2
+            }
+        }
+
+        return conf
+
     def apply_defaults(self, default_config=None):
-        return self._recursive_update(self._config, default_config or DEFAULT_SERVER_CONFIG)
+        return self._recursive_update(self._config, default_config or ServerConfig.get_default())
 
     def _recursive_update(self, config, defaults):
         def update(d, u):
